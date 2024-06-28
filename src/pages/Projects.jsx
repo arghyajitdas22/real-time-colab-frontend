7c3534d877dffc6a20ac2c3699239797f1575932
Arghyajit Das <116450956+arghyajitdas22@users.noreply.github.com>
2024-06-28 14:20:10 +0530

    integrated create team and project and changed create modal

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Teamlayout from "../components/teams/layouts/Team.layout";
-import Sidebar from "../components/sidebar/Sidebar";
+import { useParams } from "react-router-dom";
 const Projects = () => {
+  const { teamId } = useParams();
+  useEffect(() => {
+    localStorage.setItem("teamId", teamId);
+  }, [teamId]);
+
   return (
     <Teamlayout>
-      <Sidebar />
       <h1>Will display the projects that the user is assigned in that team.</h1>
     </Teamlayout>
   );
